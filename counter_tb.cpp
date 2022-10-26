1d14d367971a79937eb2a7f1ca01636c23ae2779
YasserHassan03 <116260803+YasserHassan03@users.noreply.github.com>
2022-10-26 10:52:25 +0100

    Add files via upload

--- counter_tb.cpp
+++ counter_tb.cpp
@@ -1,7 +1,7 @@
 #include "Vcounter.h"
 #include "verilated.h"
 #include "verilated_vcd_c.h"
-
+#include "vbuddy.cpp"
 int main(int argc, char **argv, char **env){
     int i;
     int clk;
@@ -13,6 +13,9 @@ int main(int argc, char **argv, char **env){
     VerilatedVcdC* tfp = new VerilatedVcdC;
     top->trace (tfp, 99);
     tfp->open ("counter.vcd");
+    //init Vbuddy
+    if (vbdOpen()!=1) return(-1);
+    vbdHeader("Lab 1: Counter");
 
     // initialise simulation inputs
     top->clk = 1;
@@ -28,18 +31,25 @@ int main(int argc, char **argv, char **env){
          
 
         }
+        /// ++++ Send count value to Vbuddy
+        vbdHex(4,(int(top->count)>>16)& 0xF);
+        vbdHex(3,(int(top->count)>>8)& 0xF);
+        vbdHex(2,(int(top->count)>>4)&0xF);
+        vbdHex(1, int(top->count) & 0xF);
+        vbdCycle(i+1);
+        // ---- end of Vbuddy output section
+
         top->rst = (i <2) | (i == 15); 
     
-        top->en = (i >4);
+        top->en = vbdFlag();
         
-        if( (i > 13) && (i < 1)){
-            top->en = 0;
-        } 
+        
         
         
     
         if (Verilated::gotFinish()) exit(0);
     }
+    vbdClose(); // ++++
     tfp->close();
     exit(0);
 }
